44d5f56312a4ac2912a80890067a2eef90aa2a93
dsquareindia <ashu.9412@gmail.com>
2016-10-09 16:02:51 +0530

    slight indentation correction

--- credentials.py
+++ credentials.py
@@ -1,6 +1,6 @@
 creds_dict = {
 	'consumer_key': 'ENTER CONSUMER KEY',
-    'consumer_secret': 'ENTER CONSUMER SECRET KEY',
+     'consumer_secret': 'ENTER CONSUMER SECRET KEY',
 	'access_token_key': 'ENTER ACCESS TOKEN KEY',
 	'access_token_secret': 'ENTER SECRET ACCESS TOKEN KEY'
 }
